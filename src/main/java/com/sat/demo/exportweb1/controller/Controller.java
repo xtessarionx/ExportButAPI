135bd13cb74b6454804c6a43bc0813d66a613792
Sathit Sriwan <sathit.sriwan@gmail.com>
2023-03-27 17:25:38 +0700

    Add log style tua mae

--- src/main/java/com/sat/demo/exportweb1/controller/Controller.java
+++ src/main/java/com/sat/demo/exportweb1/controller/Controller.java
@@ -4,6 +4,7 @@ import com.sat.demo.exportweb1.model.Customer;
 import com.sat.demo.exportweb1.service.CustomerService;
 import com.sat.demo.exportweb1.utill.CSVGenerator;
 import com.sat.demo.exportweb1.utill.PDFGenerator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,8 +13,8 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
+@Slf4j
 @org.springframework.stereotype.Controller
 public class Controller {
     @Autowired
@@ -24,28 +25,39 @@ public class Controller {
 
     @GetMapping("/export/csv")
     public void exportToCSV(HttpServletResponse response) throws IOException{
+        log.info("Exporting into csv~~~");
         response.setContentType("text/csv");
         response.addHeader("Content-Disposition", "attachment; filename=\"customers.csv\"");
         csvGenerator.writeToCSV(customerService.getCustomersList(),response.getWriter());
+        log.info("Exported csv~~~");
+
     }
     @GetMapping("/export/txt")
     public void exportToTXT(HttpServletResponse response) throws IOException{
+        log.info("Exporting into txt~~~");
         response.setContentType("text/plain");
         response.addHeader("Content-Disposition", "attachment; filename=\"customers.txt\"");
         response.setCharacterEncoding("UTF-8");
         csvGenerator.writeToCSV(customerService.getCustomersList(),response.getWriter());
+        log.info("Exported txt~~~");
     }
 
     @GetMapping("/export/pdf")
     public void exportToPDF(HttpServletResponse response) throws IOException {
+        log.info("Exporting into pdf~~~");
         response.setContentType("application/pdf");
         DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-DD:HH:MM:SS");
         String headerkey = "Content-Disposition";
         String headervalue = "attachment; filename=pdf_" + dateFormat.format(new Date()) + ".pdf";
         response.setHeader(headerkey,headervalue);
-
-
         PDFGenerator generator = new PDFGenerator();
-        generator.exportToPDF(customerService.getCustomersList(),response);
+        try{
+            generator.exportToPDF(customerService.getCustomersList(),response);
+        }
+        catch (Exception e){
+            log.error("Something error while exporting");
+        }
+        log.info("Exported pdf~~~");
+
     }
 }
--- src/main/java/com/sat/demo/exportweb1/utill/CSVGenerator.java
+++ src/main/java/com/sat/demo/exportweb1/utill/CSVGenerator.java
@@ -1,6 +1,7 @@
 package com.sat.demo.exportweb1.utill;
 
 import com.sat.demo.exportweb1.model.Customer;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.springframework.stereotype.Component;
@@ -8,18 +9,21 @@ import org.springframework.stereotype.Component;
 import java.io.Writer;
 import java.util.List;
 
+@Slf4j
 @Component
 public class CSVGenerator {
     public void writeToCSV(List<Customer> customers, Writer writer){
+
         try{
             CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT);
+            printer.printRecord("Id","firstName","lastName","email","gender","contactNo","country","dob");
             for(Customer customer:customers){
-                System.out.println(customer.toString());
                 printer.printRecord(customer.getId(),customer.getFirstName(),customer.getLastName(),customer.getEmail()
                 ,customer.getGender(),customer.getContactNo(),customer.getCountry(),customer.getDob());
             }
         }catch (Exception e){
-            e.printStackTrace();
+            log.error("Something wrong while exporting",e);
         }
+
     }
 }
